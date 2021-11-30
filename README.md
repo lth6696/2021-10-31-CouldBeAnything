374f9b1f9ea7eeac5be26d139bc8499d267615d8
lth6696 <lth9804@bupt.edu.cn>
2021-11-30 12:06:34 +0800

    start the rwka

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# 2021-10-31-QueueSysTools
-A tool for solving queueing problem.
+# CouldBeAnything
+This repository is a temporary place for the program that will grow up in the future.
--- src/main.py
+++ src/main.py
@@ -1,11 +1,50 @@
-import pbty_dist as pd
-import queueing_sys as qs
+"""
+The RWKA model for the PuLP modeller
+
+Authors: LTH6696 2021
+"""
+import numpy as np
+from pulp import *
 
 
-def function():
-    rv = pd.ContinuousProbabilityDistributions.erlang_distribution(1.99)
-    qs.MarkovChainMonteCarlo.MetropolisHastingsSampler(rv)
+def rwka_function():
+    prob = LpProblem("RWKA", LpMaximize)
+
+    # Creates a list of all nodes
+    vars = generate_variables(6)
+
+    # vars = LpVariable(name="LigthPaths", lowBound=0, upBound=None, cat=LpInteger)
+    v = LpVariable.dicts("Ligthpaths", vars, 0)
+
+    # The objective function is added to 'prob' first
+    prob += (lpSum(v[i] for i in vars))
+
+    # The follow constraints are entered
+    prob += (lpSum())
+
+    # The problem is solved using PuLP's choice of Solver
+    prob.solve()
+
+    print("Status:", LpStatus[prob.status])
+    for v in prob.variables():
+        print(v.name, "=", v.varValue)
+
+def generate_variables(nodes):
+    var = ['{}{}'.format(i, j) for i in range(nodes) for j in range(nodes)]
+    return var
+
+def generate_network_topology(nodes=None, conn_matrix=None):
+    if nodes is None:
+        nodes = 6
+    if conn_matrix is None:
+        conn_matrix = np.matrix('0 1 0 1 0 0; '
+                                '1 0 1 1 0 0; '
+                                '0 1 0 0 1 1; '
+                                '1 1 0 0 1 0; '
+                                '0 0 1 1 0 1; '
+                                '0 0 1 0 1 0')
+    return (nodes, conn_matrix)
 
 
 if __name__ == '__main__':
-    function()
+    rwka_function()
