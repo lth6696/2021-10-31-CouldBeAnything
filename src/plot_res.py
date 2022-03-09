cb91c4c5532e39a5f743b55528ef6f7ea145cdc0
lth6696 <lth9804@bupt.edu.cn>
2022-03-09 18:01:37 +0800

    change the result figures

--- src/plot_res.py
+++ src/plot_res.py
@@ -37,7 +37,7 @@ def plot_successed_route():
     plt.errorbar(x, SPF_SUCCESS['mean'][0], yerr=[spf_err_neg, spf_err_pos], label='SPF',
                  color='#FFC000', ls='-', lw=0.5,
                  marker='o', mec='#FFC000', mfc='#FFC000', mew=0.5, ms=2)
-    plt.xlabel('Traffic load')
+    plt.xlabel('Workload')
     plt.ylabel('Success allocation rate (%)')
     plt.xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
     plt.yticks(rotation='vertical')
@@ -81,7 +81,7 @@ def plot_shortest_hop_first():
     plt.errorbar(x, SPF_HOP_DIS['mean'][0], yerr=[spf_err_neg, spf_err_pos], label='SPF',
                  color='#FFC000', ls='-', lw=0.5,
                  marker='o', mec='#FFC000', mfc='#FFC000', mew=0.5, ms=2)
-    plt.xlabel('Traffic load')
+    plt.xlabel('Workload')
     plt.ylabel('Hop')
     plt.xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
     plt.yticks(rotation='vertical')
@@ -102,40 +102,40 @@ def plot_max_res_utilization():
     x = [(i + 1) * 0.05 for i in range(20)]
     shf_data_neg = [SHF_RES['data_mean'][0][i] - SHF_RES['data_min'][0][i] for i in range(len(SHF_RES['data_mean'][0]))]
     shf_data_pos = [SHF_RES['data_max'][0][i] - SHF_RES['data_mean'][0][i] for i in range(len(SHF_RES['data_mean'][0]))]
-    shf_key_neg = [SHF_RES['key_mean'][0][i] - SHF_RES['key_min'][0][i] for i in range(len(SHF_RES['key_mean'][0]))]
-    shf_key_pos = [SHF_RES['key_max'][0][i] - SHF_RES['key_mean'][0][i] for i in range(len(SHF_RES['key_mean'][0]))]
+    # shf_key_neg = [SHF_RES['key_mean'][0][i] - SHF_RES['key_min'][0][i] for i in range(len(SHF_RES['key_mean'][0]))]
+    # shf_key_pos = [SHF_RES['key_max'][0][i] - SHF_RES['key_mean'][0][i] for i in range(len(SHF_RES['key_mean'][0]))]
     mru_data_neg = [MRU_RES['data_mean'][0][i] - MRU_RES['data_min'][0][i] for i in range(len(MRU_RES['data_mean'][0]))]
     mru_data_pos = [MRU_RES['data_max'][0][i] - MRU_RES['data_mean'][0][i] for i in range(len(MRU_RES['data_mean'][0]))]
-    mru_key_neg = [MRU_RES['key_mean'][0][i] - MRU_RES['key_min'][0][i] for i in range(len(MRU_RES['key_mean'][0]))]
-    mru_key_pos = [MRU_RES['key_max'][0][i] - MRU_RES['key_mean'][0][i] for i in range(len(MRU_RES['key_mean'][0]))]
+    # mru_key_neg = [MRU_RES['key_mean'][0][i] - MRU_RES['key_min'][0][i] for i in range(len(MRU_RES['key_mean'][0]))]
+    # mru_key_pos = [MRU_RES['key_max'][0][i] - MRU_RES['key_mean'][0][i] for i in range(len(MRU_RES['key_mean'][0]))]
     spf_err_neg = [SPF_RES['mean'][0][i] - SPF_RES['min'][0][i] for i in range(len(SPF_RES['mean'][0]))]
     spf_err_pos = [SPF_RES['max'][0][i] - SPF_RES['mean'][0][i] for i in range(len(SPF_RES['mean'][0]))]
 
     excellent_val = [SHF_RES['data_mean'][0][i] - MRU_RES['data_mean'][0][i] for i in range(len(SHF_RES['data_mean'][0]))]
     print('Data: SHF better than MRU', np.mean(excellent_val))
-    excellent_val = [SHF_RES['key_mean'][0][i] - MRU_RES['key_mean'][0][i] for i in range(len(SHF_RES['key_mean'][0]))]
-    print('Key: SHF better than MRU', np.mean(excellent_val))
+    # excellent_val = [SHF_RES['key_mean'][0][i] - MRU_RES['key_mean'][0][i] for i in range(len(SHF_RES['key_mean'][0]))]
+    # print('Key: SHF better than MRU', np.mean(excellent_val))
     excellent_val = [SPF_RES['mean'][0][i] - MRU_RES['data_mean'][0][i] for i in range(len(SPF_RES['mean'][0]))]
     print('Data: SPF better than MRU', np.mean(excellent_val))
 
     plot_style()
-    plt.errorbar(x, SHF_RES['data_mean'][0], yerr=[shf_data_neg, shf_data_pos], label='SHF-data',
+    plt.errorbar(x, SHF_RES['data_mean'][0], yerr=[shf_data_neg, shf_data_pos], label='SHF',
                  color='#4472C4', ls='-', lw=0.5,
                  marker='o', mec='#4472C4', mfc='#4472C4', mew=0.5, ms=2)
-    plt.errorbar(x, SHF_RES['key_mean'][0], yerr=[shf_key_neg, shf_key_pos], label='SHF-key',
-                 color='#B4C7E7', ls='-', lw=0.5,
-                 marker='o', mec='#B4C7E7', mfc='#B4C7E7', mew=0.5, ms=2)
-    plt.errorbar(x, MRU_RES['data_mean'][0], yerr=[mru_data_neg, mru_data_pos], label='MRU-data',
+    # plt.errorbar(x, SHF_RES['key_mean'][0], yerr=[shf_key_neg, shf_key_pos], label='SHF-key',
+    #              color='#B4C7E7', ls='-', lw=0.5,
+    #              marker='o', mec='#B4C7E7', mfc='#B4C7E7', mew=0.5, ms=2)
+    plt.errorbar(x, MRU_RES['data_mean'][0], yerr=[mru_data_neg, mru_data_pos], label='MRU',
                  color='#C55A11', ls='-', lw=0.5,
                  marker='o', mec='#C55A11', mfc='#C55A11', mew=0.5, ms=2)
-    plt.errorbar(x, MRU_RES['key_mean'][0], yerr=[mru_key_neg, mru_key_pos], label='MRU-key',
-                 color='#F4B183', ls='-', lw=0.5,
-                 marker='o', mec='#F4B183', mfc='#F4B183', mew=0.5, ms=2)
+    # plt.errorbar(x, MRU_RES['key_mean'][0], yerr=[mru_key_neg, mru_key_pos], label='MRU-key',
+    #              color='#F4B183', ls='-', lw=0.5,
+    #              marker='o', mec='#F4B183', mfc='#F4B183', mew=0.5, ms=2)
     plt.errorbar(x, SPF_RES['mean'][0], yerr=[spf_err_neg, spf_err_pos], label='SPF',
                  color='#FFC000', ls='-', lw=0.5,
                  marker='o', mec='#FFC000', mfc='#FFC000', mew=0.5, ms=2)
-    plt.xlabel('Traffic load')
-    plt.ylabel('Resource utilization')
+    plt.xlabel('Workload')
+    plt.ylabel('Bandwidth utilization')
     plt.xticks([0, 0.2, 0.4, 0.6, 0.8, 1])
     plt.yticks(rotation='vertical')
     plt.tight_layout()
