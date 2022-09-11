928cea2389856224156085775051e483cbd07440
刘天和 <lth9804@bupt.edu.cn>
2022-09-11 09:15:06 +0800

    2022.09.11

--- source/Result/ResultPlot.py
+++ source/Result/ResultPlot.py
@@ -142,14 +142,99 @@ class ResultPresentation(object):
 
 
 if __name__ == '__main__':
-    result_json = json.load(open('../results_for_sci.json'))
-    var = 'network_capacity'
-    # ResultPresentation().plot_line_graph_for_icocn([[np.sum(row) for row in result_json[solver][var]] for solver in ['FF-SL', 'FF-ML', 'SLF-ML']],
-    # ResultPresentation().plot_line_graph_for_icocn([[row[2] for row in result_json[solver][var]] for solver in ['FF-SL', 'FF-ML', 'SLF-ML']],
-    ResultPresentation().plot_line_graph_for_icocn([result_json[solver][var] for solver in ['FF-SL', 'FF-ML', 'SLF-ML']],
-                                                   x=[i for i in range(1, 41)],
-                                                   xlabel='Number of Traffic Matrix',
-                                                   ylabel='Throughput (Tb/s)',
-                                                   fsize=(8.6, 6.2)
-                                                   # fsize=(6, 4.5)
-                                                   )
+    # result_json = json.load(open('../results_for_sci.json'))
+    # var = 'network_capacity'
+    # # ResultPresentation().plot_line_graph_for_icocn([[np.sum(row) for row in result_json[solver][var]] for solver in ['FF-SL', 'FF-ML', 'SLF-ML']],
+    # # ResultPresentation().plot_line_graph_for_icocn([[row[2] for row in result_json[solver][var]] for solver in ['FF-SL', 'FF-ML', 'SLF-ML']],
+    # ResultPresentation().plot_line_graph_for_icocn([result_json[solver][var] for solver in ['FF-SL', 'FF-ML', 'SLF-ML']],
+    #                                                x=[i for i in range(1, 41)],
+    #                                                xlabel='Number of Traffic Matrix',
+    #                                                ylabel='Throughput (Tb/s)',
+    #                                                fsize=(8.6, 6.2)
+    #                                                # fsize=(6, 4.5)
+    #                                                )
+
+    style(8.6, 6.2)
+    marker = ['o', 's', 'X', '^', 'p']
+    line = ['-', '--', ':', '-.']
+    map_vir = cm.get_cmap(name='gist_rainbow')
+    # solver = ['LBMS', 'LSMS']
+    K = [4, 8, 12, 16]
+    # y1 = [98, 96.2, 93.1, 92]
+    # y2 = [92.3, 90, 86.6, 80.8]
+    # plt.plot(K, y1, marker=marker[0], ms=2, ls=line[0], lw=0.5, color=map_vir(1/2),
+    #          label=solver[0])
+    # plt.plot(K, y2, marker=marker[1], ms=2, ls=line[1], lw=0.5, color=map_vir(2/2),
+    #          label=solver[1])
+    # plt.yticks(rotation='vertical')
+    # plt.xlabel("Number of Traffic Matrices")
+    # plt.ylabel("Success Mapping Rate (%)")
+    # plt.xticks(K)
+    # plt.yticks([i * 5 + 80 for i in range(0, 5)])
+    # plt.grid(True, ls=':', lw=0.5, c='#d5d6d8')
+    # plt.tight_layout()
+    # plt.legend()
+    # plt.show()
+
+    """
+    # Throughput
+    y1 = [[230, 414, 556, 731],
+          [231, 478, 701, 903],
+          [244, 475, 726, 948]]
+    y2 = [[230, 418, 616, 729],
+          [223, 420, 605, 743],
+          [214, 429, 591, 718]]
+    """
+    """
+    # Hops
+    y1 = [[1.90, 1.89, 1.79, 1.71],
+          [1.72, 1.75, 1.81, 1.81],
+          [1.66, 1.70, 1.80, 1.92]]
+    y2 = [[1.92, 1.85, 1.86, 1.75],
+          [1.89, 1.81, 1.85, 1.75],
+          [1.87, 1.86, 1.80, 1.78]]
+    """
+    """
+    # Lightpath Utilization
+    y1 = [[35.7, 64.2, 78.7, 88.6],
+          [15.8, 35.0, 59.5, 78.0],
+          [6.5, 14.0, 28.7, 51.8]]
+    y2 = [[20.2, 37.7, 54.0, 58.1],
+          [20.0, 36.3, 51.1, 59.9],
+          [20.8, 36.2, 50.6, 59.0]]
+          
+    plt.bar(np.array(K) - 1.1, y1[0], width=0.3, label='LBMS', color=map_vir(1/2))
+    plt.bar(np.array(K) - 0.8, y2[0], width=0.3, label='LSMS', color=map_vir(2/2))
+    plt.bar(np.array(K) - 0.3, y1[1], width=0.3, label='LBMS', color=map_vir(1/2))
+    plt.bar(np.array(K) + 0, y2[1], width=0.3, label='LSMS', color=map_vir(2/2))
+    plt.bar(np.array(K) + 0.5, y1[2], width=0.3, label='LBMS', color=map_vir(1/2))
+    plt.bar(np.array(K) + 0.8, y2[2], width=0.3, label='LSMS', color=map_vir(2/2))
+    plt.yticks(rotation='vertical')
+    plt.xlabel("Number of Traffic Matrices")
+    plt.ylabel("Hop")
+    plt.xticks(K)
+    plt.yticks([i * 0.5 for i in range(2, 6)])
+    plt.grid(True, ls=':', lw=0.5, c='#d5d6d8')
+    plt.tight_layout()
+    plt.legend(solver)
+    plt.show()
+    """
+    """"""
+    # ILP vs Heuristic
+    y = [[98.0, 96.2, 93.1, 92.0],
+         [92.3, 90.0, 86.6, 80.8],
+         [96.98, 95.18, 92.01, 86.18],
+         [97.08, 95.42, 92.82, 85.33],
+         [91.82, 88.07, 81.51, 76.19]]
+    solver = ['ILP-LBMS', 'ILP-LSMS', 'SASMA-ISv1', 'SASMA-ISv2', 'SASMA-LSMS']
+    for i, scheme in enumerate(y):
+        plt.plot(K, scheme, marker=marker[i], ms=2, ls=line[i%4], lw=0.5, color=map_vir((i+1)/len(y)), label=solver[i])
+    plt.yticks(rotation='vertical')
+    plt.xlabel("Number of Traffic Matrices")
+    plt.ylabel("Success Mapping Rate (%)")
+    plt.xticks(K)
+    plt.yticks([i * 5 for i in range(15, 21)])
+    plt.grid(True, ls=':', lw=0.5, c='#d5d6d8')
+    plt.tight_layout()
+    plt.legend(solver)
+    plt.show()
